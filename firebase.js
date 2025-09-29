5219b43ac9b4bab06cecbc547119e2dcd793e27a
ezekill35 <135133397+ezekill35@users.noreply.github.com>
2025-09-29 09:26:15 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCIo7CBX5jzAGlDFBu0mMb6BFfUsecaf7I",
   authDomain: "discovery-pets.firebaseapp.com",
   projectId: "discovery-pets",
-  storageBucket: "discovery-pets.appspot.com", // corregido
+  storageBucket: "discovery-pets.appspot.com",
   messagingSenderId: "481355972999",
   appId: "1:481355972999:web:a073cc5af230b32f4c5322",
   measurementId: "G-W5RGYVTW3V"
@@ -15,6 +15,7 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 // Servicios principales
-const auth = firebase.auth();      // para login/registro
-const db = firebase.firestore();   // para datos del negocio
+const auth = firebase.auth();      // 🔑 Login/registro
+const db = firebase.firestore();   // 📦 Base de datos en la nube
+
 
