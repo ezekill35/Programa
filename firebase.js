1f37da2fdf3c4cae58b1d9675ad5288a0db415a4
ezekill35 <135133397+ezekill35@users.noreply.github.com>
2025-09-29 14:07:28 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,24 @@
-// Import Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDoc, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+// ==============================================
+// Configuración de Firebase para Discovery Pets
+// ==============================================
 
-// Config
 const firebaseConfig = {
   apiKey: "AIzaSyCIo7CBX5jzAGlDFBu0mMb6BFfUsecaf7I",
   authDomain: "discovery-pets.firebaseapp.com",
   projectId: "discovery-pets",
-  storageBucket: "discovery-pets.firebasestorage.app",
+  storageBucket: "discovery-pets.appspot.com",
   messagingSenderId: "481355972999",
   appId: "1:481355972999:web:a073cc5af230b32f4c5322",
   measurementId: "G-W5RGYVTW3V"
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Servicios principales
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+
 
 
