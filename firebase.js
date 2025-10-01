56e815882481b3561b7e9b7b0ce0a3b6272e1b0f
ezekill35 <135133397+ezekill35@users.noreply.github.com>
2025-10-01 08:13:00 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,21 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// 🔑 Tu configuración Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCIo7CBX5jzAGlDFBu0mMb6BFfUsecaf7I",
   authDomain: "discovery-pets.firebaseapp.com",
   projectId: "discovery-pets",
-  storageBucket: "discovery-pets.firebasestorage.app",
+  storageBucket: "discovery-pets.appspot.com",
   messagingSenderId: "481355972999",
   appId: "1:481355972999:web:a073cc5af230b32f4c5322",
   measurementId: "G-W5RGYVTW3V"
 };
 
 // Inicializar Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
+export { auth };
 
 
