99143a8d1b1d1096dd0329b2b6d5bd1ea3afbdbf
ezekill35 <135133397+ezekill35@users.noreply.github.com>
2025-10-01 11:43:12 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -14,9 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 export const db = getFirestore(app);
-
 
 
 
