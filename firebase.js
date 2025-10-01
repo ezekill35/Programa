fd940584e3c492a62285b028ab489981623e219d
ezekill35 <135133397+ezekill35@users.noreply.github.com>
2025-10-01 11:20:17 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,28 +1,24 @@
-// firebase.js
-// Importa solo los SDK que necesitas
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+<!-- firebase.js -->
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 
-// Configuración de Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyCIo7CBX5jzAGlDFBu0mMb6BFfUsecaf7I",
-  authDomain: "discovery-pets.firebaseapp.com",
-  projectId: "discovery-pets",
-  storageBucket: "discovery-pets.appspot.com",
-  messagingSenderId: "481355972999",
-  appId: "1:481355972999:web:a073cc5af230b32f4c5322",
-  measurementId: "G-W5RGYVTW3V"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyCIo7CBX5jzAGlDFBu0mMb6BFfUsecaf7I",
+    authDomain: "discovery-pets.firebaseapp.com",
+    projectId: "discovery-pets",
+    storageBucket: "discovery-pets.firebasestorage.app",
+    messagingSenderId: "481355972999",
+    appId: "1:481355972999:web:5f5fa07f75b3fc9f4c5322",
+    measurementId: "G-0WMLRY8FGM"
+  };
 
-// Inicializa Firebase
-const app = initializeApp(firebaseConfig);
-
-// Servicios que usarás
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { app, auth, db };
+  // Inicializa Firebase
+  const app = initializeApp(firebaseConfig);
+  export const auth = getAuth(app);
+  export const db = getFirestore(app);
+</script>
 
 
 
