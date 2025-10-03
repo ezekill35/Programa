a9b15a6b4fa35e2743c3536fa6cd25cf6b7cb21b
ezekill35 <135133397+ezekill35@users.noreply.github.com>
2025-10-03 13:29:14 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
-import { getAuth, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -20,6 +20,8 @@ export const db = getFirestore(app);
 export const cerrarSesion = async () => {
   await signOut(auth);
 };
+
+
 
 
 
