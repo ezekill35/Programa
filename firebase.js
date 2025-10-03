8fa560a5e666d9c93ffa61ad2c60856ff69b1815
ezekill35 <135133397+ezekill35@users.noreply.github.com>
2025-10-03 17:22:55 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,9 @@
-// =======================================
-// 🐾 Firebase Config - Discovery Pets
-// =======================================
-
 // Importar Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
 import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 
-// Configuración Firebase
+// Configuración de tu proyecto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCIo7CBX5jzAGlDFBu0mMb6BFfUsecaf7I",
   authDomain: "discovery-pets.firebaseapp.com",
@@ -25,13 +21,9 @@ export const db = getFirestore(app);
 
 // Función para cerrar sesión
 export const cerrarSesion = async () => {
-  try {
-    await signOut(auth);
-    alert("Sesión cerrada");
-    window.location.href = "index.html";
-  } catch (error) {
-    console.error("Error al cerrar sesión:", error);
-  }
+  await signOut(auth);
+  alert("Sesión cerrada");
+  window.location.href = 'index.html';
 };
 
 
