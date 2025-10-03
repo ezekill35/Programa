7bc2e5f9a04f896a28bd4e665dcea8ec3068b7f8
ezekill35 <135133397+ezekill35@users.noreply.github.com>
2025-10-03 17:33:14 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,25 @@
-// firebase.js
+// Importar Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
 import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 
+// Configuración Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCIo7CBX5jzAGlDFBu0mMb6BFfUsecaf7I",
   authDomain: "discovery-pets.firebaseapp.com",
   projectId: "discovery-pets",
   storageBucket: "discovery-pets.appspot.com",
   messagingSenderId: "481355972999",
-  appId: "1:481355972999:web:a073cc5af230b32f4c5322"
+  appId: "1:481355972999:web:a073cc5af230b32f4c5322",
+  measurementId: "G-W5RGYVTW3V"
 };
 
-const app = initializeApp(firebaseConfig);
+// Inicializar Firebase
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
+// Función cerrar sesión
 export const cerrarSesion = async () => {
   await signOut(auth);
 };
