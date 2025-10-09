86c6b506fe126a03eff4e7bbbc2407d01f8d0d79
ezekill35 <135133397+ezekill35@users.noreply.github.com>
2025-10-09 11:27:19 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,8 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
-// Configuración de tu proyecto
 const firebaseConfig = {
     apiKey: "AIzaSyCIo7CBX5jzAGlDFBu0mMb6BFfUsecaf7I",
     authDomain: "discovery-pets.firebaseapp.com",
@@ -14,10 +13,10 @@ const firebaseConfig = {
     measurementId: "G-0WMLRY8FGM"
 };
 
-// Inicializar Firebase
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+
 
 
 
