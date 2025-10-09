1a04690fec12d62b9d69f6aa53132628cc277de3
ezekill35 <135133397+ezekill35@users.noreply.github.com>
2025-10-09 09:41:56 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
-import firebase from "https://www.gstatic.com/firebasejs/7.20.0/firebase-app.js";
-import "https://www.gstatic.com/firebasejs/7.20.0/firebase-auth.js";
-import "https://www.gstatic.com/firebasejs/7.20.0/firebase-firestore.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCIo7CBX5jzAGlDFBu0mMb6BFfUsecaf7I",
@@ -8,14 +9,18 @@ const firebaseConfig = {
     projectId: "discovery-pets",
     storageBucket: "discovery-pets.appspot.com",
     messagingSenderId: "481355972999",
-    appId: "1:481355972999:web:5f5fa07f75b3fc9f4c5322"
+    appId: "1:481355972999:web:5f5fa07f75b3fc9f4c5322",
+    measurementId: "G-0WMLRY8FGM"
 };
 
-firebase.initializeApp(firebaseConfig);
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const db = firebase.firestore();
-export default firebase;
+// Exportar auth y firestore para usarlos en otros módulos
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+
 
 
 
