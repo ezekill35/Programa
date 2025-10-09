d4a222e34c781f563a5b96c24e962a47003acbe8
ezekill35 <135133397+ezekill35@users.noreply.github.com>
2025-10-08 21:22:39 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+// Configuración Firebase
 var firebaseConfig = {
   apiKey: "AIzaSyCIo7CBX5jzAGlDFBu0mMb6BFfUsecaf7I",
   authDomain: "discovery-pets.firebaseapp.com",
@@ -7,13 +8,14 @@ var firebaseConfig = {
   appId: "1:481355972999:web:5f5fa07f75b3fc9f4c5322",
   measurementId: "G-0WMLRY8FGM"
 };
-
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Servicios
+// Variables globales
 var auth = firebase.auth();
 var db = firebase.firestore();
+
+
 
 
 
