88a5e86ab7c69971bead7351c2ecbe0cf2b8407d
ezekill35 <135133397+ezekill35@users.noreply.github.com>
2025-10-08 21:46:54 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,17 @@
-// Configuración Firebase
+// Configuración de Firebase 7.20.0
 var firebaseConfig = {
-  apiKey: "AIzaSyCIo7CBX5jzAGlDFBu0mMb6BFfUsecaf7I",
-  authDomain: "discovery-pets.firebaseapp.com",
-  projectId: "discovery-pets",
-  storageBucket: "discovery-pets.appspot.com",
-  messagingSenderId: "481355972999",
-  appId: "1:481355972999:web:5f5fa07f75b3fc9f4c5322",
-  measurementId: "G-0WMLRY8FGM"
+    apiKey: "AIzaSyCIo7CBX5jzAGlDFBu0mMb6BFfUsecaf7I",
+    authDomain: "discovery-pets.firebaseapp.com",
+    databaseURL: "",
+    projectId: "discovery-pets",
+    storageBucket: "discovery-pets.appspot.com",
+    messagingSenderId: "481355972999",
+    appId: "1:481355972999:web:5f5fa07f75b3fc9f4c5322",
+    measurementId: "G-0WMLRY8FGM"
 };
+
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
-
-// Variables globales
 var auth = firebase.auth();
 var db = firebase.firestore();
 
