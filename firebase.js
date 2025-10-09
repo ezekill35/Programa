0db369c42712eb26b01220154f35e6f5114a4108
ezekill35 <135133397+ezekill35@users.noreply.github.com>
2025-10-08 20:43:13 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,28 @@
 <!-- firebase.js -->
+<!-- Agregar esto antes de dashboard.js -->
 <script src="https://www.gstatic.com/firebasejs/7.20.0/firebase-app.js"></script>
 <script src="https://www.gstatic.com/firebasejs/7.20.0/firebase-auth.js"></script>
 <script src="https://www.gstatic.com/firebasejs/7.20.0/firebase-firestore.js"></script>
 
 <script>
+  // Configuración Firebase
   var firebaseConfig = {
     apiKey: "AIzaSyCIo7CBX5jzAGlDFBu0mMb6BFfUsecaf7I",
     authDomain: "discovery-pets.firebaseapp.com",
     projectId: "discovery-pets",
     storageBucket: "discovery-pets.appspot.com",
     messagingSenderId: "481355972999",
-    appId: "1:481355972999:web:5f5fa07f75b3fc9f4c5322"
+    appId: "1:481355972999:web:5f5fa07f75b3fc9f4c5322",
+    measurementId: "G-0WMLRY8FGM"
   };
+
+  // Inicializar Firebase
   firebase.initializeApp(firebaseConfig);
 
-  var auth = firebase.auth();
-  var db = firebase.firestore();
+  // Referencias
+  const auth = firebase.auth();
+  const db = firebase.firestore();
 </script>
-
 
 
 
